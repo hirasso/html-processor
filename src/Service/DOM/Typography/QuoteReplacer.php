8ba4e348a7254b499347e284d7aa347164c22e42
Rasso Hilber <mail@rassohilber.com>
2026-01-22 17:12:40 +0100

    Optimize quote localizer logic

--- src/Service/DOM/Typography/QuoteReplacer.php
+++ src/Service/DOM/Typography/QuoteReplacer.php
@@ -26,21 +26,41 @@ final readonly class QuoteReplacer
      */
     public function apply(string $text): string
     {
-        $text = str_replace(['‘', '’', '‚', '‹', '›'], "'", $text);
-        $text = str_replace(['“', '”', '„', '«', '»'], '"', $text);
+        $text = $this->normalize($text, ['‘', '’', '‚', '‹', '›'], "'");
+        $text = $this->normalize($text, ['“', '”', '„', '«', '»'], "\"");
 
-        $text = $this->replace($text, "'", $this->single);
-        $text = $this->replace($text, '"', $this->double);
+        $text = $this->localize("'", $this->single, $text);
+        $text = $this->localize('"', $this->double, $text);
+
+        return $text;
+    }
+
+    /**
+     * @param string[] $quotes
+     */
+    private function normalize(string $text, array $quotes, string $replace): string
+    {
+        /** at the start of a string */
+        $text = preg_replace(array_map(fn (string $s) => "/(?<!\p{L})$s/u", $quotes), $replace, $text) ?? $text;
+        /** at the end of a string */
+        $text = preg_replace(array_map(fn (string $s) => "/$s(?!\p{L})/u", $quotes), $replace, $text) ?? $text;
         return $text;
     }
 
     /**
      * Replace a string with a callback
+     * @param string|string[] $search
      */
-    private function replace(string $text, string $search, Closure $callback): string
+    private function localize(string|array $search, Closure $callback, string $text): string
     {
+        /** always convert to array */
+        $search = array_map(
+            fn (string $s) => "/(?<!\p{L})$s(.*?)$s(?!\p{L})/u",
+            is_array($search) ? $search : [$search]
+        );
+
         return preg_replace_callback(
-            "/(?<!\p{L})$search(.*?)$search(?!\p{L})/u",
+            $search,
             fn ($matches) => ($callback)($matches[1]),
             $text
         ) ?? $text;
